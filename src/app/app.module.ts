7d4e0055d1b04ac17690801b5dd8dcb42a940223
Eyup KORURER <150202017@kocaeli.edu.tr>
2019-01-29 18:52:00 +0300

    fromtend modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import {MatInputModule, MatCardModule, MatButtonModule} from '@angular/material'
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +12,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
